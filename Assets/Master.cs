3b4296a170dbf5d4b60785cd0cf72c94d0274c0b
Adam Biggs <wareaglebiggs@gmail.com>
2024-10-28 18:51:38 -0500

    Made camera follow

--- Assets/Master.cs
+++ Assets/Master.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Master : MonoBehaviour
 {
+    public GameObject CameraToFollow;
+
+    public GameObject PlayerObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class Master : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CameraToFollow.transform.position = new Vector3(PlayerObj.transform.position.x + 10,
+            PlayerObj.transform.position.y + 18, PlayerObj.transform.position.z - 10);
     }
 }
